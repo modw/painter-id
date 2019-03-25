763d548e831f7758267542558fa2e9d5d2c6c750
Marcio <modw@users.noreply.github.com>
2019-03-25 14:09:26 -0400

    added y_list option

--- data_processing.py
+++ data_processing.py
@@ -12,24 +12,24 @@ from sklearn.preprocessing import RobustScaler
 def data_to_array(data_address):
     """Imports x,y,z txt data, grabs and reshapes z values,
     returns ndarray with appropriate shape.
-    
+
     Parameters
     ----------
     data_address: str
         file of xyz data
-    
+
     Returns
     ----------
     data: 2d ndarray with z values"""
     # import data
     data = pd.read_csv(data_address, header=None,
-                  names = ['x','y','z'])
+                       names=['x', 'y', 'z'])
     # find out length and width
     width, length = data['x'].nunique(), data['y'].nunique()
-    # get z-data as reshaped ndarray 
+    # get z-data as reshaped ndarray
     z_arr = data['z'].values.reshape(length, width)
     # set negative values no NaNs
-    z_arr[z_arr<0] = None
+    z_arr[z_arr < 0] = None
     return z_arr
 
 
@@ -37,7 +37,7 @@ def scale_data(data, scaler):
     """Takes data and sklearn scaler as input, returns scaled data.
     The function's purpose is to deal with reshaping."""
     # fit scaler to flattened data
-    data_scaled = scaler.fit_transform(data.reshape(-1,1))
+    data_scaled = scaler.fit_transform(data.reshape(-1, 1))
     # returns scaled data in original dimensions
     return data_scaled.reshape(data.shape)
 
@@ -48,7 +48,7 @@ def scale_data(data, scaler):
 def get_patches(data, patch_size, shift=True, dropna=True):
     """Splits the data uniformly in square patches of 
     size patch_size. Returns new array with each patch.
-    
+
     Parameters
     ----------
     data : ndarray
@@ -59,42 +59,44 @@ def get_patches(data, patch_size, shift=True, dropna=True):
         If True, subtract off mean of each patch
     dropna: bool
         If True, discard patches with any NaN element
-        
+
     Returns
     ----------
     patches: 3d array of dimensions (n_patches, patch_size, patch_size)
     """
-    patches = extract_patches(data, patch_size, 
+    patches = extract_patches(data, patch_size,
                               extraction_step=patch_size)
-    patches = patches.reshape(-1,patch_size,patch_size)
+    patches = patches.reshape(-1, patch_size, patch_size)
     if dropna:
         # drop patches with nans in them
-        patches = patches[~np.isnan(patches).any(axis=(1,2))]
+        patches = patches[~np.isnan(patches).any(axis=(1, 2))]
     if shift:
         # take mean of each patch and reshape it for subtaction
-        means = np.mean(patches, axis=(1,2)).reshape(-1,1,1)
+        means = np.mean(patches, axis=(1, 2)).reshape(-1, 1, 1)
         patches -= means
     return patches
 
 
-def get_X_y(data_list, shuffle=True):
+def get_X_y(data_list, y_list=None, shuffle=True):
     """
     Parameters
     ---------
     data_tuple: list of arrays
         A list with arrays of patches
+    y_list: None or list of length len(data_list)
+        A list of the labels associated with data_list. Defaults to None.
     shuffle: bool
         Whether or not to shuffle the elements in X,y
     Returns
     ---------
     X, y: X is an array with the patches and y has the corresponding labels as ints
     """
-    y_list = [None]*len(data_list)
-    
-    for i, data in enumerate(data_list):
-        y_list[i] = i*np.ones(len(data))
+    if y_list is None:
+        y_list = [None]*len(data_list)
+        for i, data in enumerate(data_list):
+            y_list[i] = i*np.ones(len(data))
 
-    y = np.concatenate(y_list).reshape(-1,1)
+    y = np.concatenate(y_list).reshape(-1, 1)
     X = np.concatenate(data_list)
     if shuffle:
         shuffled_idx = np.arange(len(y))
@@ -113,20 +115,23 @@ def get_X_y(data_list, shuffle=True):
 # step 3: get_patches from scaled data, size, shifting and dropping nans should be options
 # step 4: produce X and y arrays and return them
 
-def processing_pipeline(files, patch_size, scaler=None,
-                       shift=True, dropna=True, shuffle=True):
+def processing_pipeline(files, patch_size, y_list=None, scaler=None,
+                        shift=True, dropna=True, shuffle=True):
     """
     Function to process data for training.
     Returns X and y arrays. The returned arrays are of same size
     as the z dimension of the files, but the algorithm needs 3 times
     that amount of memory.
-    
+
     Parameters
     ---------
     files: list of str
         Files of xyz data
     patch_size : integer
         The size of the patch square side
+    y_list: None or list of length len(files)
+        A list of the labels associated with files. If None (Default), 
+        the function will assign a different label to each file.
     shift: bool
         If True, subtract off mean of each patch
     dropna: bool
@@ -135,7 +140,7 @@ def processing_pipeline(files, patch_size, scaler=None,
         Scaler to scale the data. Defaults to RobustScaler
     shuffle: bool
         Whether or not to shuffle the elements in X,y
-   
+
     Returns
     ---------
     X, y: X is an array with the patches and y has the corresponding labels as ints
@@ -152,7 +157,7 @@ def processing_pipeline(files, patch_size, scaler=None,
             data_arrs[i] = scale_data(data_arrs[i], scaler)
         # get patches
         patches_list[i] = get_patches(data_arrs[i], patch_size,
-                                     shift, dropna)
+                                      shift, dropna)
     # get X,y arrays
-    X, y = get_X_y(patches_list, shuffle)
-    return X,y
+    X, y = get_X_y(patches_list, y_list, shuffle)
+    return X, y
